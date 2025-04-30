a7844f7685002a50c232a0453ed53496d4535194
reyvelour <shaabanm457@gmail.com>
2025-04-30 22:06:04 +0300

    made it pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+   let sum = 0;
+   let small = num1;
+   let big = num2;
 
+   if (num1 > num2) {
+    big = num1;
+    small = num2;
+   }
+   
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) ||
+    !Number.isInteger(num2) || typeof num1 !== "number" ||
+    typeof num2 !== "number" ) {
+    return "ERROR";
+   } else {
+    for (i = small ; i <= big; i++)
+     sum += i;
+   }  
+   return sum;
 };
 
 // Do not edit below this line
