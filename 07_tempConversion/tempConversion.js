09374d623024c51b1190c1206d26a0f717738865
reyvelour <shaabanm457@gmail.com>
2025-04-30 22:25:55 +0300

    made it pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let cel = (fah - 32) * (5 / 9);
+  return Number(cel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fah = (cel * (9 / 5)) + 32;
+  return Number(fah.toFixed(1));
 };
 
 // Do not edit below this line
