ffad59a91640249808c6d1dd63a3383a5ca9632e
reyvelour <shaabanm457@gmail.com>
2025-04-30 22:05:31 +0300

    made it pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(cats, ...args) {
+   let dogs = [];
+   
+    for (cat of cats) {
+        if (!args.includes(cat)) {
+            dogs.push(cat)
+        } 
+    } return dogs;
 };
+
+// [3,4,5,1], [1,2]
 
 // Do not edit below this line
 module.exports = removeFromArray;
