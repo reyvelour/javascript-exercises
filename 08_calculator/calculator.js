c3f05540f8453a990a389b0341b782a4d1c9b449
reyvelour <shaabanm457@gmail.com>
2025-05-02 16:55:16 +0300

    made it pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(total, a) {
+  return total + a;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce(function(total, a) {
+  return total * a;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = num; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
