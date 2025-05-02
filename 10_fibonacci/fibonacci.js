81aa41c02bdaf3de4bdb5bcef95f83efa71336f7
reyvelour <shaabanm457@gmail.com>
2025-05-02 18:25:06 +0300

    made it pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   num = Number(num);
+   
+  if (num < 0) return 'OOPS';
+  if (num == 0 ) return 0;
+  
+  fib = [1,1];
 
+  for ( let i=0 ; i <= num ; i++) {
+    fib.push(fib[i]+fib[i+1]);
+  }
+
+  return fib[num -1];
+  
 };
 
 // Do not edit below this line
