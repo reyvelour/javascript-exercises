be80d02c4982a9c8fac9e9c4c8dc1c2897fe6139
reyvelour <shaabanm457@gmail.com>
2025-05-02 18:41:17 +0300

    made it pass all the tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+ return object.map(function(book) {
+    return book.title;
+  });
 };
 
 // Do not edit below this line
